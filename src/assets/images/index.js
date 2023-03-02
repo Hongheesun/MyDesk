5ca280b21b0c4d3299b9a0e85c79c374de171128
hongheesun <hia990618@gmail.com>
2023-03-02 19:06:06 +0900

    add: img 추가

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+import NoteImg from "./note.png";
+import ClipImg from "./clip.png";
+import EarphonesImg from "./earphones.png";
+import PencilImg from "./pencil.png";
+import PhoneImg from "./phone.png";
+
+export { NoteImg, ClipImg, EarphonesImg, PencilImg, PhoneImg };
