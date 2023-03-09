e63209a73d9bbf123227f126022a0aa438eeb799
hongheesun <hia990618@gmail.com>
2023-03-09 11:58:13 +0900

    주석 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./component/Main/Main";
 import Note from "./component/OpenNote/OpenNote";
-// import Message from "./component/Message/Message";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/note" element={<Note />} />
-        {/* <Route path="/message" element={<Message />} /> */}
       </Routes>
     </BrowserRouter>
   );
