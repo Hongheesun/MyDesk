a29ac6dfd7ea3e9ea400ce350d55cff288cfc0d2
hongheesun <hia990618@gmail.com>
2023-03-09 12:03:42 +0900

    배포

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Main from "./component/Main/Main";
 import Note from "./component/OpenNote/OpenNote";
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/MyDesk">
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/note" element={<Note />} />
